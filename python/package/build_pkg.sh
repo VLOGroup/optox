4b788af84d0332444ed95995477aadd1f9c95333
Erich Kobler <erich.kobler@icg.tugraz.at>
2018-08-29 10:05:26 +0200

    added check for commands in sh scripts

--- python/package/build_pkg.sh
+++ python/package/build_pkg.sh
@@ -1,8 +1,26 @@
+#!/bin/sh
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+check_command()
+{
+  COMMAND=$@
+  eval "$COMMAND"
+
+  rc=$?
+  if [ $rc -ne 0 ]; then
+	printf "${RED} ERROR:${NC} $COMMAND\n"
+    exit $rc
+  fi
+}
+
 # copy the libraries
-cp ../../lib/python/PyNablaOperator.so ./optopy/nabla/PyNablaOperator.so
+check_command cp ../../lib/python/PyNablaOperator.so ./optopy/nabla/PyNablaOperator.so
 
 # build the package
-python setup.py sdist bdist_wheel
+check_command python setup.py sdist bdist_wheel
 
 # install
 #pip install dist/optotf-0.1.dev0-cp36-cp36m-linux_x86_64.whl --upgrade
--- tensorflow/package/build_pkg.sh
+++ tensorflow/package/build_pkg.sh
@@ -1,11 +1,28 @@
+#!/bin/sh
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+check_command()
+{
+  COMMAND=$@
+  eval "$COMMAND"
+
+  rc=$?
+  if [ $rc -ne 0 ]; then
+	printf "${RED} ERROR:${NC} $COMMAND\n"
+    exit $rc
+  fi
+}
+
 # copy the libraries
-cp ../../lib/tf/TfActivationOperators.so ./optotf/activations/TfActivationOperators.so
-cp ../../lib/tf/TfMetamorphosisOperator.so ./optotf/interpolation/TfMetamorphosisOperator.so
-cp ../../lib/tf/TfRotateFiltersOperator.so ./optotf/interpolation/TfRotateFiltersOperator.so
+check_command cp ../../lib/tf/TfActivationOperators.so ./optotf/activations/TfActivationOperators.so
+check_command cp ../../lib/tf/TfMetamorphosisOperator.so ./optotf/interpolation/TfMetamorphosisOperator.so
+check_command cp ../../lib/tf/TfRotateFiltersOperator.so ./optotf/interpolation/TfRotateFiltersOperator.so
 
 # build the package
-python setup.py sdist bdist_wheel
+check_command python setup.py sdist bdist_wheel
 
 # install
 #pip install dist/optotf-0.1.dev0-cp36-cp36m-linux_x86_64.whl --upgrade
-
